6907b4b75042f1f5e347e82aba0a6f6d9e49d36c
Bristisul123 <151063935+Bristisul123@users.noreply.github.com>
2024-03-25 22:59:05 +0600

    Add files via upload

---
+++ week2/Day-4/a1.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define int long long
+#define all(x) x.begin(), x.end()
+#define yes cout<<"YES"<<"\n"
+#define no cout<<"NO"<<"\n"
+#define nl "\n"
+#define lower(x) transform(x.begin(), x.end(), x.begin(), ::tolower)
+#define upper(x) transform(x.begin(), x.end(), x.begin(), ::toupper)
+typedef unsigned long long  ull;
+using namespace std;
+
+
+void solve()
+{ 
+
+  int n , m; cin>>n>>m;
+  vector<int>a(n);
+  for(int i=0;i<n;i++)
+    cin>>a[i];
+  int l = 0, r = 0, ans = 0, sum = 0;
+
+  while(l<n)
+  {
+     sum+=a[l];
+    if(sum<=m)
+       ans = max(ans, l-r+1);
+ else
+   {
+     sum -= a[r];
+     r++;
+   }
+   l++;
+  }
+cout<< ans;
+ 
+}
+int32_t main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+    int T=1;// cin>>T;
+    while(T--)
+    {
+        solve();
+    }
+    return 0;
+}
+    
+
+
