8a3e0f8a95927f820c3ee273c4d5a971b72b6962
Bristisul123 <151063935+Bristisul123@users.noreply.github.com>
2024-04-15 22:00:31 +0600

    Add files via upload

---
+++ week4/Day-4/i.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define int long long
+#define all(x) x.begin(), x.end()
+#define yes cout<<"YES"<<"\n"
+#define no cout<<"NO"<<"\n"
+#define nl "\n"
+#define lower(x) transform(x.begin(), x.end(), x.begin(), ::tolower)
+#define upper(x) transform(x.begin(), x.end(), x.begin(), ::toupper)
+typedef unsigned long long  ull;
+using namespace std;
+
+
+void solve()
+{ 
+
+  int n; cin>>n;
+  int n2=1<<(int)(log2(n));
+  //cout<< n2 << " ...";
+   int a=n-n2;
+   int b=n2;
+  cout<< a <<" "<<b<<nl;
+}
+int32_t main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+    int T=1; cin>>T;
+    while(T--)
+    {
+        solve();
+    }
+    return 0;
+}
+    
+
+
